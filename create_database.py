1c2a50395c08c185bc1558fcbe306ef567c836f0
Kumar Saurav <athena.saurav@gmail.com>
2022-05-07 23:07:40 +0530

    Add files via upload

--- create_database.py
+++ create_database.py
@@ -3,23 +3,21 @@ pymysql.install_as_MySQLdb()
 import MySQLdb
 import pandas as pd
 from sqlalchemy import create_engine, types
-import mysql.connector as msql
+import mysql.connector as mysql
 from mysql.connector import Error
+from actions import config
 try:
-    conn = msql.connect(host="localhost" , user="root" , passwd="12345" ,db="mysql")#give ur username, password
+    conn = mysql.connect(host=config.host , user=config.user , passwd=config.password ,db="mysql")#give ur username, password
     if conn.is_connected():
         cursor = conn.cursor()
-        cursor.execute("CREATE DATABASE rasa_db")
+        cursor.execute("CREATE DATABASE {}".format(config.database))
         print("Database is created")
-        # cursor.execute("CREATE TABLE rikjai(id VARCHAR(255),Outdoor_Indoor VARCHAR(255), Category VARCHAR(255), Title VARCHAR(255), Date VARCHAR(255), Timeslot VARCHAR(255), Duration VARCHAR(255), Destination VARCHAR(255), District VARCHAR(255), min_nos_people VARCHAR(255), max_nos_people VARCHAR(255), vacancy VARCHAR(255), Price_per_head VARCHAR(255), Tel VARCHAR(255), Website  VARCHAR(255))")
-        # print("Table is created....")
-
 except Error as e:
     print("Error while connecting to MySQL", e)
 
 try:
-    engine = create_engine('mysql://root:12345@localhost/rasa_db') # enter your password and database names here
-    df = pd.read_csv("email_pass.csv",sep=',',quotechar='\'',encoding='utf8') # Replace Excel_file_name with your excel sheet name
+    engine = create_engine('mysql://{}:{}@{}/{}'.format(config.user, config.password, config.host, config.database)) # enter your password and database names here
+    df = pd.read_csv("email_pass.csv",sep=',',quotechar='\'',encoding='utf8') # Replace CSV_File_name with your excel sheet name
     df.to_sql('login',con=engine,index=False,if_exists='append')
     print("Login Table is created")
 except Error as e:
@@ -27,8 +25,8 @@ except Error as e:
 
 
 try:
-    engine = create_engine('mysql://root:12345@localhost/rasa_db') # enter your password and database names here
-    df = pd.read_csv("incident.csv",sep=',',quotechar='\'',encoding='utf8') # Replace Excel_file_name with your excel sheet name
+    engine = create_engine('mysql://{}:{}@{}/{}'.format(config.user, config.password, config.host, config.database)) # enter your password and database names here
+    df = pd.read_csv("incident.csv",sep=',',quotechar='\'',encoding='utf8') # Replace CSV_File_name with your excel sheet name
     df.to_sql('incident',con=engine,index=False,if_exists='append')
     print("Incident Table is created")
 except Error as e:
